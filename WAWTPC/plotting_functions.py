dee8205e8a866a4975d56a4d1aafc136d45efe99
mwbaj <149197527+mwbaj@users.noreply.github.com>
2024-01-07 11:51:52 +0100

    Update plotting_functions.py
    Fix to the previous commit

--- WAWTPC/plotting_functions.py
+++ WAWTPC/plotting_functions.py
@@ -39,8 +39,8 @@ def plotEndpoints(data, iProj, axis, label, color):
         axis.plot(uvwt[3], uvwt[iProj], marker='.', markersize=20, alpha=0.8, color=color)
 ###################################################
 ###################################################
-def plotEvent(data, model):
-    '''(optional) axlim: list of lists of lists with axes limits for three plots [[[x0min,x0max],[y0min,y0max]],[[x1min,x1max],[y1min,y1max]],[[x2min,x2max],[y2min,y2max]]]'''      
+def plotEvent(data, model, axlim=False):
+    '''axlim: list of lists of lists with axes limits for three plots [[[x1min,x1max],[y1min,y1max]],[[x2min,x2max],[y2min,y2max]],[[x3min,x3max],[y3min,y3max]]]'''
     #data indexing: data[features/label][element in batch][index in features/label]
     projNames = ("U", "V", "W")
     fig, axes = plt.subplots(1,3, figsize=(28,10))
